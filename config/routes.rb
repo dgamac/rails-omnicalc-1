3e7c6a6d1eb5604024db325129a16310abb5348e
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-08 03:20:28 +0000

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "home", :action => "home_page"})
   
+  get("/square/new", {:controller => "home", :action => "square_num"})
+
+  get("/square/results", {:controller => "home", :action => "square_result"})
+
+  get("/square_root/new", {:controller => "home", :action => "square_root"})
+
+  get("/square_root/results", {:controller => "home", :action => "square_root_results"})
+
+  get("/payment/new", {:controller => "home", :action => "new_payment"})
+
+  get("/payment/results", {:controller => "home", :action => "payment_result"})
+
+  get("/random/new", {:controller => "home", :action => "random_num"})
+
+  get("/random/results", {:controller => "home", :action => "random_result"})
 end
